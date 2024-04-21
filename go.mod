f86b0df7cb7e85d096738c99283d6b8935b21e2e
Pandika <pandika@transfez.com>
2024-04-22 00:08:56 +0700

    go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module my-project
 
 go 1.22.1
+
+require github.com/pandikaadi/gologger v0.1.0
--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "my-project/helper"
+import (
+// "my-project/helper"
+// "github.com/softchris/math"
+// "fmt"
+// "github.com/gosimple/slug"
+	"github.com/pandikaadi/gologger"
+)
 
 func main() {
-	helper.Help()
-	helper.helping()
+	// helper.Help()
+	// // helper.helping()
+	// fmt.Println(math.Add(1,2))
+	// originalString := "This is a string that needs to be slugged!"
+	// sluggedString := slug.Make(originalString)
+	
+	// fmt.Printf("Original string: %s\n", originalString)
+	// fmt.Printf("Slug: %s\n", sluggedString)
+	logger.Log("eee")
+
 }
